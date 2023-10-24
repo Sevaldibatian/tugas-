2214fd5941b466261a4ad959df14aaf9dca72f89
ASUS TUF <gilangananta706@gmail.com>
2023-10-24 15:02:51 +0700

    tugas pemograman 4.1 dan 4.2

---
+++ tugas42.m
@@ -0,0 +1,12 @@
+clear
+clc
+%matrik 5x5
+A = [ 1 2 3 4 5 ; 5 4 3 2 1 ; 2 3 4 5 1 ; 3 4 5 1 2 ; 4 5 1 2 3]
+%matrik 3x3 dengan elemen nol
+B = [0 0 0 ; 0 0 0 ; 0 0 0]
+%matrik 4x4 elemen 1
+C = [ 1 1 1 1 ; 1 1 1 1 ; 1 1 1 1 ; 1 1 1 1 ]
+% matrik 3x4 elemen terdistribusi antara 0 dan 1
+D = [ 1 0 0 1 ; 0 1 0 1 ; 1 0 1 0 ]
+%matrix 3x4  elemen terdistribusi normal
+E = [ 1 4 0 2 ; 2 3 1 0 ; 1 3 4 4 ]
